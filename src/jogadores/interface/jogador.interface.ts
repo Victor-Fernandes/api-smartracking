aa2e90c214149e269f04b68ea8318758722e1a1a
Victor Fernandes <victor.fernandes.07@gmail.com>
2022-02-13 20:33:35 -0300

    criacao da interface e dto jogador

---
+++ src/jogadores/interface/jogador.interface.ts
@@ -0,0 +1,9 @@
+export interface jogador {
+  readonly _id: string;
+  readonly telefoneCelular: string;
+  readonly email: string;
+  nome: string;
+  ranking: string;
+  posicao: number;
+  urlFotoJogador: string;
+}
--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criarJogador.dto';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
-
   @Post()
-  async criarAtualizarJogador() {
-     return JSON.stringify({
-       nome: 'NICE'
-     })
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { email } = criarJogadorDto;
+    return JSON.stringify(email);
   }
 }
